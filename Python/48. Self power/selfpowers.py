ecb3fb39d7a314448eff4cdf0aa63907c6abecf7
Paulo Hernane <paulohernane10@gmail.com>
2019-10-04 12:07:30 -0300

    fix 11 digits problem

--- Python/48. Self power/selfpowers.py
+++ Python/48. Self power/selfpowers.py
@@ -4,6 +4,6 @@ for i in range(1, 1001):
 sumSerie = str(sumSerie)
 tam = len(sumSerie)
 serie = ""
-for i in range((tam-11), tam):
+for i in range((tam-11), tam-1):
     serie += sumSerie[i]
 print(serie)
