6f4acf851b9e5328b92e813b2a164dff08515e5b
amannirala13 <asdevofficial@gmail.com>
2019-10-06 21:22:17 +0530

    Fixed issue in 05 script

--- Python/5. Smallest multiple/smallest_multi.py
+++ Python/5. Smallest multiple/smallest_multi.py
@@ -20,7 +20,8 @@ from datetime import datetime            #Importing datetime for calculating tim
 def smallest():             #Returns the smallest +ve number that is evenly divisible to all the numbers between 1 to 20
     start_time = datetime.now()
     num = 20
-    check = [20, 19, 18, 17, 16, 14, 13, 11]
+    #check = [20, 19, 18, 17, 16, 14, 13, 11]
+    check = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
     while True:
         for i in range (0, len(check)):
             if(num%check[i] !=0):
