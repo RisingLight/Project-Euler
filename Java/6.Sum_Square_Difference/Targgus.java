2b4dfc1c303aac1278eccd57650dc7e2b68c5576
Targgus <jlhenson13@gmail.com>
2019-10-08 06:36:22 -0600

    Updated Folder and File name

---
+++ Java/6.Sum_Square_Difference/Targgus.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SumSquareDifference {
+
+    public static void sumsquarediff() {
+        Scanner input = new Scanner(System.in);
+        int sum_of_squares = 0;
+        int squares_of_sums = 0;
+
+        int num = input.nextInt();
+
+        for (int i = 1; i < num+1; i++) {
+            sum_of_squares += i * i;
+            squares_of_sums += i;
+        }
+
+        squares_of_sums *= squares_of_sums;
+
+        System.out.println(squares_of_sums - sum_of_squares);
+
+    }
+}
+
+
+
