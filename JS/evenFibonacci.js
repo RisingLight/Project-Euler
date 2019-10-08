362061c60510d4354f5ba761da5e212cac24556a
cilasmarques <cilas.marques@ccc.ufcg.edu.br>
2019-10-08 00:25:25 -0300

    Adding javaScript code for 02 Even Fibonacci numbers

---
+++ JS/evenFibonacci.js
@@ -0,0 +1,17 @@
+function fibonacci(num) {
+    if (num <= 1) return 1;
+  
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+console.log (fibonacci(0))
+console.log (fibonacci(1))
+console.log (fibonacci(2))
+console.log (fibonacci(3))
+console.log (fibonacci(4))
+console.log (fibonacci(5))
+console.log (fibonacci(6))
+console.log (fibonacci(7))
+console.log (fibonacci(8))
+console.log (fibonacci(9))
+console.log (fibonacci(10))
