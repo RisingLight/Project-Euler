019a9f76c4fecb453e5337ff969bcfe29ef22b88
Vipul Bajaj <vipulbjj@gmail.com>
2019-10-09 22:08:10 +0530

    Update Vipul-Bajaj.py

--- Python/07.10001st_prime/Vipul-Bajaj.py
+++ Python/07.10001st_prime/Vipul-Bajaj.py
@@ -1,5 +1,7 @@
-# Euler problem 7
-# Print the 10001st Prime Number
+# Euler 7
+# Problem Name - 10001st_prime 
+# Description - Print the 10001st Prime Number
+
 # Maximum number upto which you can query.
 MAX = 10000007 
 # Function to return nth prime number if between 1 and MAX range, returns the nth prime number if 1 <= n  < MAX otherwise None
