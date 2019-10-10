6dfd4e3b993fb2755cb7d1a28b3fe7391991f502
Ajay Bhave <ajaybhave108@gmail.com>
2019-10-10 21:29:21 +0530

    Update Ajay Bhave.cpp

--- CPP/08.Largest product in a series/Ajay Bhave.cpp
+++ CPP/08.Largest product in a series/Ajay Bhave.cpp
@@ -1,1 +1,32 @@
-.
+// Project Euler : Problem Number 08
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,k;
+        cin>>n>>k;
+        string s;
+        cin>>s;
+        int a[n];
+        for(ll i=0;i<n;i++)
+        {
+            a[i]=(int)(s[i])-48;
+        }
+        ll m=INT_MIN;
+        for(ll i=0;i<n-k+1;i++)
+        {
+            ll s=1;
+            for(ll j=0;j<k;j++)
+            s*=a[j+i];
+            m=max(m,s);
+        }
+        cout<<m<<endl;
+    }
+    return(0);
+}
+
