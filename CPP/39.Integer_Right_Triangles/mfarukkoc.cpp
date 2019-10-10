ad791ace91a4503ef316d21141c6a18ad32c6231
mfarukkoc <42540003+mfarukkoc@users.noreply.github.com>
2019-10-10 21:59:55 +0300

    Update mfarukkoc.cpp
    Added problem statement as comment.

--- CPP/39.Integer_Right_Triangles/mfarukkoc.cpp
+++ CPP/39.Integer_Right_Triangles/mfarukkoc.cpp
@@ -1,3 +1,10 @@
+/*
+Euler Problem 39
+Integer Right Triangles:
+If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
+{20,48,52}, {24,45,51}, {30,40,50}
+For which value of p ≤ 1000, is the number of solutions maximised?
+*/
 #include <iostream>
 #include <string>
 using namespace std;
