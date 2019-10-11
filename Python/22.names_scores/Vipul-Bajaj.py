8169628679b9bf9d734af19b65a2a7378c076ced
Vipul Bajaj <vipulbjj@gmail.com>
2019-10-11 20:44:54 +0530

    Problem 22 done in Python3

---
+++ Python/22.names_scores/Vipul-Bajaj.py
@@ -0,0 +1,17 @@
+# Euler 22
+def get_total_sum_of_names(names_file_path):
+    name_file = open(names_file_path,"r")
+    names = name_file.read()
+    names_list = names.split(",")
+    names_list.sort()
+    total_names_sum = 0
+    for index,name in enumerate(names_list):
+        name_sum = 0
+        for ch in name:
+            name_sum+=(ord(ch)-64)
+        total_names_sum+=(name_sum*index)
+
+    return total_names_sum
+
+if __name__ == "__main__":
+    print(get_total_sum_of_names("p022_names.txt"))
