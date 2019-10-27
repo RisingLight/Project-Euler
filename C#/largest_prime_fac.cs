0d966b1b18026f23632cfdfd38e407be46e0d8ad
Prhmma <prhmma@gmail.com>
2019-10-28 00:18:42 +0330

    create largest_prime_fac

---
+++ C#/largest_prime_fac.cs
@@ -0,0 +1,25 @@
+using System;
+namespace largest_prime_fac
+{
+    class largest_prime_fac
+    {
+        public static void Main()
+        {
+            long num = 600851475143;
+            int count = 3;
+            while (num > 1)
+            {
+                if (num % count == 0)
+                {
+                    num /= count;
+                }
+                else
+                {
+                    count += 2;
+                }
+            }
+            Console.WriteLine("The largest prime factor of the number {0} is {1} ", 600851475143, count);
+            Console.ReadKey();
+        }
+    }
+}
