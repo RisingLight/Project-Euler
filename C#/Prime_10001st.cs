411fe658e2b1a793fe3923800c041df7782da773
Prhmma <prhmma@gmail.com>
2019-10-28 00:47:25 +0330

    Create Prime_10001st.cs

---
+++ C#/Prime_10001st.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _Prime_10001st
+{
+    class Prime_10001st
+    {
+        public static void Main()
+        {
+            List<long> primeNumbers = new List<long>() { 2 };
+            for (long i = 3; i < long.MaxValue; i += 2)
+            {
+                if (!primeNumbers.Any(p => (i % p) == 0))
+                {
+                    primeNumbers.Add(i);
+                    if (primeNumbers.Count == 10001)
+                    {
+                        Console.WriteLine(i);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
