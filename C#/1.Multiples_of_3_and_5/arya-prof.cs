5c5f360543b928c5a34ab1087f5265d2188757aa
arya <arya.valizadeh@outlook.com>
2019-10-27 21:58:41 +0330

    1 done in C#

---
+++ C#/1.Multiples_of_3_and_5/arya-prof.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace _5_multiples
+{
+    public class MainClass
+    {
+        public static void Main()
+        {
+            int currentNumber = 1000;
+            int allSum = 0;
+
+            currentNumber = currentNumber - 1;
+            while (currentNumber > 0)
+            {
+                if ( (currentNumber % 3 == 0) | (currentNumber % 5 == 0))
+                {
+                    allSum = allSum + currentNumber;
+                }
+                currentNumber = currentNumber - 1;
+            }
+            Console.WriteLine(allSum);
+        }
+    }
+}
