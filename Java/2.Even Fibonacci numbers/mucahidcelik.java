714182b44159a8c9915b5322f293a6f9581ec5e5
Mücahid Çelik <31987698+mucahidcelik@users.noreply.github.com>
2019-10-28 10:44:08 +0300

    problem_2 done in Java (#172)

---
+++ Java/2.Even Fibonacci numbers/mucahidcelik.java
@@ -0,0 +1,14 @@
+//Euler Problem_2
+public class mucahidcelik{
+    public static void main(String []args){
+        int pre=1,post=1,sum=0 ;
+
+        while (post<=4000000){
+            if(post%2==0)
+                sum+=post;
+            post=pre+post;
+            pre=post-pre;
+        }
+        System.out.println(sum);
+	}
+}
