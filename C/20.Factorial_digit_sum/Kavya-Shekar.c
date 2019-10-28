1666c6236ebdd84d8221d0dd299a369b5f8090dd
Kavya-Shekar <kavyay2k@gmail.com>
2019-10-28 12:18:53 +0530

    Added comments for prob.20

--- C/20.Factorial_digit_sum/Kavya-Shekar.c
+++ C/20.Factorial_digit_sum/Kavya-Shekar.c
@@ -1,4 +1,9 @@
+// Problem 20 : Factorial digit sum
+// n! means n × (n − 1) × ... × 3 × 2 × 1
+// Find the sum of the digits in the number 100!
+
 #include<stdio.h>
+
 int main()
 {
     int a[200],n,counter,temp;
